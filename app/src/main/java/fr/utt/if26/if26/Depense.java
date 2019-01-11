a019791dc592c5ee2cbe8042eb6d9e89ba3be006
mike3022 <625353945@qq.com>
2019-01-11 15:19:37 +0100

    add monthyearpicker and pie chart

--- app/src/main/java/fr/utt/if26/if26/Depense.java
+++ app/src/main/java/fr/utt/if26/if26/Depense.java
@@ -1,6 +1,8 @@
 package fr.utt.if26.if26;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,7 +11,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.List;
 
--- app/src/main/java/fr/utt/if26/if26/MainActivity.java
+++ app/src/main/java/fr/utt/if26/if26/MainActivity.java
@@ -1,30 +1,46 @@
 package fr.utt.if26.if26;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
+import com.anychart.enums.Align;
+import com.anychart.enums.LegendLayout;
 
 import org.w3c.dom.Text;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private String monthStr = "";
+    private MonthYearPickerDialog pickerDialog = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final thingBD bd = thingBD.getInstance(this, thingBD.DATABASE_NAME, null, 1);
+
         if (getIntent().getExtras() != null) {
             monthStr = getIntent().getExtras().getString("month");
         }
@@ -33,11 +49,38 @@ public class MainActivity extends AppCompatActivity {
             DateFormat dateFormat = new SimpleDateFormat("MM/yyyy");
             monthStr = dateFormat.format(date);
         }
-        EditText monthTxt = findViewById(R.id.main_month);
+        final TextView monthTxt = findViewById(R.id.main_month);
         monthTxt.setText(monthStr);
+        monthTxt.setOnClickListener(listener1);
+        pickerDialog = new MonthYearPickerDialog();
+        pickerDialog.setListener(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int i2) {
+                monthTxt.setText(month + "/" + year);
+                monthStr = monthTxt.getText().toString();
+                double revenue = bd.getRevenue(year, month);
+                TextView revenueTv = findViewById(R.id.main_revenue);
+                revenueTv.setText(String.valueOf(revenue));
+                ImageView ivrev = findViewById(R.id.main_ivrevenue);
+                ivrev.setOnClickListener(listener2);
+
+                ImageView ivdep = findViewById(R.id.main_ivdepenses);
+                ivdep.setOnClickListener(listener3);
+
+                double depenses = bd.calcuteDepenses(year, month);
+                depenses = Math.floor(depenses * 100) / 100;
+                TextView depenseTv = findViewById(R.id.main_depenses);
+                depenseTv.setText(String.valueOf(depenses));
+
+                double reste = revenue - depenses;
+                reste = Math.floor(reste * 100) / 100;
+                TextView resteTv = findViewById(R.id.main_reste);
+                resteTv.setText(String.valueOf(reste));
+            }
+        });
         int year = Integer.valueOf(monthStr.split("/")[1]);
         int month = Integer.valueOf(monthStr.split("/")[0]);
-        thingBD bd = thingBD.getInstance(this, thingBD.DATABASE_NAME, null, 1);
+
         double revenue = bd.getRevenue(year, month);
         TextView revenueTv = findViewById(R.id.main_revenue);
         revenueTv.setText(String.valueOf(revenue));
@@ -57,43 +100,67 @@ public class MainActivity extends AppCompatActivity {
         TextView resteTv = findViewById(R.id.main_reste);
         resteTv.setText(String.valueOf(reste));
 
-        Button ok = findViewById(R.id.main_ok);
-        ok.setOnClickListener(listener1);
+        List<thing> things = bd.getAllThings(year, month);
+        double aliment = 0;
+        double habillement = 0;
+        double transport = 0;
+        double habitation = 0;
+        double divertissement = 0;
+        double autre = 0;
+        for (thing t :
+                things) {
+            switch(t.getCat()) {
+                case "Aliment" :
+                    aliment += t.getPrix();
+                    break;
+                case "Habillement" :
+                    habillement += t.getPrix();
+                    break;
+                case "Transport" :
+                    transport += t.getPrix();
+                    break;
+                case "Habitation" :
+                    habitation += t.getPrix();
+                    break;
+                case "Divertissement" :
+                    divertissement += t.getPrix();
+                    break;
+                case "Autre" :
+                    autre += t.getPrix();
+                    break;
+                default : break;
+            }
+        }
+        Pie pie = AnyChart.pie();
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("Aliment", aliment));
+        data.add(new ValueDataEntry("Transport", transport));
+        data.add(new ValueDataEntry("Habitation", habitation));
+        data.add(new ValueDataEntry("Divertissement", divertissement));
+        data.add(new ValueDataEntry("Habillement", habillement));
+        data.add(new ValueDataEntry("Autre", autre));
+        pie.data(data);
+
+        pie.legend()
+                .position("center-bottom")
+                .itemsLayout(LegendLayout.HORIZONTAL)
+                .align(Align.CENTER);
+        AnyChartView anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
+        anyChartView.setChart(pie);
     }
+
     private View.OnClickListener listener1 = new View.OnClickListener()
     {
         public void onClick(View v)
         {
-            EditText monthTxt = findViewById(R.id.main_month);
-            monthStr = monthTxt.getText().toString();
-            int year = Integer.valueOf(monthStr.split("/")[1]);
-            int month = Integer.valueOf(monthStr.split("/")[0]);
-            thingBD bd = thingBD.getInstance(MainActivity.this, thingBD.DATABASE_NAME, null, 1);
-            double revenue = bd.getRevenue(year, month);
-            TextView revenueTv = findViewById(R.id.main_revenue);
-            revenueTv.setText(String.valueOf(revenue));
-            ImageView ivrev = findViewById(R.id.main_ivrevenue);
-            ivrev.setOnClickListener(listener2);
-
-            ImageView ivdep = findViewById(R.id.main_ivdepenses);
-            ivdep.setOnClickListener(listener3);
-
-            double depenses = bd.calcuteDepenses(year, month);
-            depenses = Math.floor(depenses * 100) / 100;
-            TextView depenseTv = findViewById(R.id.main_depenses);
-            depenseTv.setText(String.valueOf(depenses));
-
-            double reste = revenue - depenses;
-            reste = Math.floor(reste * 100) / 100;
-            TextView resteTv = findViewById(R.id.main_reste);
-            resteTv.setText(String.valueOf(reste));
+            pickerDialog.show(getSupportFragmentManager(), "MonthYearPickerDialog");
         }
     };
     private View.OnClickListener listener2 = new View.OnClickListener()
     {
         public void onClick(View v)
         {
-            EditText monthTxt = findViewById(R.id.main_month);
+            TextView monthTxt = findViewById(R.id.main_month);
             String monthStr = monthTxt.getText().toString();
             Intent intent = new Intent();
             intent.setClass(MainActivity.this, Revenue.class);
@@ -105,7 +172,7 @@ public class MainActivity extends AppCompatActivity {
     {
         public void onClick(View v)
         {
-            EditText monthTxt = findViewById(R.id.main_month);
+            TextView monthTxt = findViewById(R.id.main_month);
             String monthStr = monthTxt.getText().toString();
             Intent intent = new Intent();
             intent.setClass(MainActivity.this, Depense.class);
